4c52a9682455158b5a303b75f8bdd025971dd40a
nicole <nzhou30@stuy.edu>
2022-01-21 21:38:15 -0500

    Looks right but needs testing

--- v3/Hangman.java
+++ v3/Hangman.java
@@ -7,6 +7,7 @@ public class Hangman extends Chatbot{
   private int lives = 10;
   private String word = "";
   private String unguessed = "";
+  private boolean correctGuess = false;
 
   Scanner in = new Scanner (System.in);
   String statement = "";
@@ -18,7 +19,9 @@ public class Hangman extends Chatbot{
     word = dictionary[ran.nextInt(dictionary.length)];
     System.out.println("Your word has " + word.length() + " letters");
     System.out.println("You have 10 lives. Start guessing one letter at a time! Please use lowercase!");
-    unguessed = "_" * word.length();
+    while (int i = 0; i < word.length(); i += 1){
+      unguessed += "_";
+    }
     System.out.println(unguessed);
   }
 
@@ -26,8 +29,25 @@ public class Hangman extends Chatbot{
     statement = in.nextLine();
     while (int i = 0; (i < word.length()) && (lives > 0); i += 1){
       if (statement.equals(word[1])){
+        correctGuess = true;
         unguessed[i] = word[i];
       }
     }
+    if ((correctGuess == true) && (unguessed.indexOf("_") > -1)){
+      System.out.println("Ding ding! You guessed correctly!")
+      System.out.println("What you have now: " + unguessed);
+    }
+    else if ((correctGuess == true) && (unguessed.indexOf("_") == -1)){
+      System.out.println("Woohoo! You got the word!");
+    }
+    else if ((correctGuess == false) && (lives > 0)){
+      lives -= 1;
+      System.out.println("Oops, you have " + lives + "left");
+    }
+    if (lives == 0){
+      System.out.println("K.O.");
+      System.out.println("Correct answer " + word);
+    }
+    correctGuess = false;
   }
 }
